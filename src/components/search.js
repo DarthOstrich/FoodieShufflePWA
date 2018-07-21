cb6d295a6b4ffdd6e60578633acff58220e86c88
Vonetta <vonettastevenson@gmail.com>
2018-07-21 13:20:54 -0700

    home page search component

---
+++ src/components/search.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react"
+import logo from "../imgs/foodieLogo.png"
+import "../css/custom.css"
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <img src={logo} alt="Foodie Logo" />
+        </header>
+
+        <button type="button" className="red center capital">
+          Find Food!
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Search
