536534f3727f9672640105c486f36ba1dcdcfb44
Vonetta <vonettastevenson@gmail.com>
2018-07-31 19:44:49 -0700

    merged

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react"
+<<<<<<< HEAD
 import Search from "./components/Search"
+=======
+import Search from "./components/search"
+>>>>>>> home page search component
 
 class App extends Component {
   render() {
     return (
+<<<<<<< HEAD
       <React.Fragment>
         <Search />
       </React.Fragment>
+=======
+      <div>
+        <Search />
+      </div>
+>>>>>>> home page search component
     )
   }
 }
